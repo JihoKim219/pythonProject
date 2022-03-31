b31aa601d0dc6356840eb352359f8f96298d2302
JihoKim219 <102738267+JihoKim219@users.noreply.github.com>
2022-03-31 21:45:52 +0900

    Update 2 on Github branch Only

--- main.py
+++ main.py
@@ -22,3 +22,6 @@ print("Hello World Update on GitHub branch")
 
 #updates in local master branch
 print("Hello World Updates on Local Master Branch")
+
+#updates in main branch of Github
+print("Hello World Update 2 on GitHub branch Only")
